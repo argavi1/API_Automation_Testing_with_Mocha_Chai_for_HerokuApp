1664b88465efa58d587346ce742813c4bee84688
argavi <argavikoto68@gmail.com>
2023-07-06 21:25:55 +0700

    adding file booking.data; base.api; herokuapp.api

---
+++ tests/data/booking.data.js
@@ -0,0 +1,16 @@
+export const VALID_BOOKING = {
+    "firstname" : "Argavi",
+    "lastname" : "Koto",
+    "totalprice" : 150,
+    "depositpaid" : true,
+    "bookingdates" : {
+        "checkin" : "2023-07-01",
+        "checkout" : "2023-07-03"
+    },
+    "additionalneeds" : "Breakfast"
+}
+
+export const LIST_BOOKING_PARAM = {
+    checkin: "2014-03-13",
+    checkout: "2015-03-13"
+}
